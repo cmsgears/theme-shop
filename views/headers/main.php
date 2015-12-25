22a0b658d5e52c664625e92c8994e90073c141dd
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-12-25 19:43:29 +0530

    Applied minor fixes.

--- views/headers/main.php
+++ views/headers/main.php
@@ -8,13 +8,16 @@ ContentUtil::initPage( $this );
 ?>
 <meta charset="<?= $coreProperties->getCharset() ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<!-- Use minimum-scale=1.0, maximum-scale=1.0, user-scalable=no for mobile applications -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <?php if( isset( $this->params['desc'] ) ) { ?>
 	<meta name="description" content="<?=$this->params['desc']?>">
 <?php } ?>
 <?php if( isset( $this->params['meta'] ) ) { ?>
 	<meta name="keywords" content="<?=$this->params['meta']?>">
 <?php } ?>
-<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
 <?= Html::csrfMetaTags() ?>
 
 <title><?= $this->title ?></title>
